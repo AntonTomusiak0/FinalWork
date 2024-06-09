3214cf95a45c403f9e014f583ad616c6ae3ad0ac
Tomus_kd35 <Tomus_kd35@student.itstep.org>
2024-06-09 10:47:10 +0300

    Add project files.

---
+++ ComplxNum.cs
@@ -0,0 +1,58 @@
+﻿
+namespace ConsoleApp44
+{
+    public class ComplxNum
+    {
+        public double a { get; set; }
+        public double b { get; set; }
+        public ComplxNum(double firstn, double secondn)
+        {
+            a = firstn;
+            b = secondn;
+        }
+        public static ComplxNum operator +(ComplxNum c1, ComplxNum c2)
+        {
+            return new ComplxNum(c1.a + c2.a, c1.b + c2.b);
+        }
+        public static ComplxNum operator -(ComplxNum c1, ComplxNum c2)
+        {
+            return new ComplxNum(c1.a - c2.a, c1.b - c2.b);
+        }
+        public static ComplxNum operator *(ComplxNum c1, ComplxNum c2)
+        {
+            double real = c1.a * c2.a - c1.b * c2.b;
+            double imaginary = c1.a * c2.b + c1.b * c2.a;
+            return new ComplxNum(real, imaginary);
+        }
+        public static ComplxNum operator /(ComplxNum c1, ComplxNum c2)
+        {
+            double denominator = c2.a * c2.a + c2.b * c2.b;
+            double real = (c1.a * c2.a + c1.b * c2.b) / denominator;
+            double imaginary = (c1.b * c2.a - c1.a * c2.b) / denominator;
+            return new ComplxNum(real, imaginary);
+        }
+        public override string ToString()
+        {
+            return $"{a} + {b}i";
+        }
+        public static void SaveToFile(string filePath, string text)
+        {
+            using (StreamWriter writer = new StreamWriter(filePath, true))
+            {
+                writer.WriteLine(text);
+            }
+        }
+        public bool IsEqual(ComplxNum num)
+        {
+            return a == num.a && b == num.b;
+        }
+        public bool IsCnjugate(ComplxNum num)
+        {
+            return a == num.a && b == -num.b;
+        }
+        public bool IsOpposite(ComplxNum num)
+        {
+            return a == -num.a && b == -num.b;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿namespace ConsoleApp44
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ComplxNum num1 = new ComplxNum(3, 2);
+            ComplxNum num2 = new ComplxNum(3, 7);
+            string filePath = "ComplxNum.txt";
+            Console.WriteLine("Number 1: " + num1);
+            Console.WriteLine("Number 2: " + num2);
+            Console.WriteLine(num1 + num2);
+            Console.WriteLine(num1 - num2);
+            Console.WriteLine(num1 * num2);
+            Console.WriteLine(num1 / num2);
+
+            ComplxNum sum = num1 + num2;
+            ComplxNum subtr = num1 - num2;
+            ComplxNum mult = num1 * num2;
+            ComplxNum div = num1 / num2;
+            ComplxNum.SaveToFile(filePath, "Num1: " + num1);
+            ComplxNum.SaveToFile(filePath, "Num2:  " + num2);
+            ComplxNum.SaveToFile(filePath, "Addition: " + sum);
+            ComplxNum.SaveToFile(filePath, "Subtraction:  " + subtr);
+            ComplxNum.SaveToFile(filePath, "Multiplication:  " + mult);
+            ComplxNum.SaveToFile(filePath, "Division:  " + div);
+
+            num1.IsEqual(num2);
+            num1.IsCnjugate(num2);
+            num1.IsOpposite(num2);
+        }
+    }
+}
